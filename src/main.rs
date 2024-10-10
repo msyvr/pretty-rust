61ec4fc5e51095bfbbeb0f61ba2173fdeefe2e45
msyvr <15218145+msyvr@users.noreply.github.com>
2024-10-10 15:49:16 -0700

    mandelbrot

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod fractal;
+mod mandelbrot;
 fn main() {
     println!("\npretty things incoming...\n");
     // Generate a julia fractal.
     fractal::fractal();
+    // Generate a mandelbrot fractal.
+    mandelbrot::mandelbrot();
 }
